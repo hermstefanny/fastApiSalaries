d634449b861a6cb05cce654200406d5c5b56c834
Estefania <aida.estefania.sosa@gmail.com>
2025-07-03 15:10:27 +0200

    get and post methods without validation

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Message(BaseModel):
+    message: str
+
+
+class Salary(BaseModel):
+    salary: int
+    bonus: int
+    taxes: int
+
 
 app = FastAPI()
 
@@ -9,5 +21,18 @@ async def welcome() -> dict:
 
 
 @app.post("/message")
-async def write_message(message: str) -> dict:
-    return {"message": message}
+async def write_message(data: Message) -> dict:
+    return {"message": data.message}
+
+
+@app.get("/number/{number}")
+async def multiply_number(number: int) -> dict:
+    return {"message": f"{number} multplied by two is {number*2}"}
+
+
+@app.post("/salary_computation")
+async def compute_salary(data: Salary) -> dict:
+    if data:
+        total = data.salary + data.bonus - data.taxes
+
+    return {"message": f"Your total is {total}"}
